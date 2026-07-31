02d6a4047b50ff5a3a258b49a629f6016213b289
Amroos101 <omayowps@gmail.com>
2026-07-30 23:40:50 -0400

    Add perishable product 'eggs' to the inventory

--- script.js
+++ script.js
@@ -53,3 +53,4 @@ let product1 = new Product("chocolate" , 2.25, 40);
 let product2 = new Product("book" , 1.14, 30);
 let product3 = new Product("water" , 10, 45);
 let perishable1 = new PerishableProduct("milk", 1.50, 20, "2026-07-30");
+let perishable2 = new PerishableProduct("eggs", 2.20, 13, "2026-06-30");
